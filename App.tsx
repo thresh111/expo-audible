7c2edb96184c903c8126ad1be3f9833a163d81b1
haoran <thresh847@163.com>
2025-06-01 01:59:21 +0800

    feat: FlatList

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
-import { Image, Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import books from "./src/dummyBooks";
 import BookListItem from "./src/components/BookListItem";
 
 export default function App() {
   return (
-    <View className="bg-slate-950 flex-1 justify-center p-4">
-      <View className="flex-1 gap-4">
-        {books.map((book) => {
-          return <BookListItem key={book.id} book={book} />;
-        })}
-      </View>
+    <View className="bg-slate-950 flex-1 justify-center p-4 pt-20">
+      <FlatList
+        data={books}
+        contentContainerClassName={"gap-4"}
+        renderItem={({ item }) => <BookListItem book={item} key={item.id} />}
+      />
 
       <StatusBar style="auto" />
     </View>
