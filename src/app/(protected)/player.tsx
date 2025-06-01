b376dccdcdfdd2ab434f6a9a5758e91f8d747024
haoran <thresh847@163.com>
2025-06-01 17:15:41 +0800

    feat: 更新播放器组件，集成音频播放功能，优化播放条显示时间

--- src/app/(protected)/player.tsx
+++ src/app/(protected)/player.tsx
@@ -1,16 +1,21 @@
 import { View, Text, Pressable, Image } from "react-native";
-import { Entypo, Ionicons } from "@expo/vector-icons";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import dummyBooks from "@/dummyBooks";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PlaybackBar from "@/components/PlaybackBar";
+import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
 
 const book = dummyBooks[0];
 
 export default function Player() {
   const router = useRouter();
+
+  const player = useAudioPlayer({ uri: book.audio_url });
+  const playerStatus = useAudioPlayerStatus(player);
+
   return (
-    <SafeAreaView className={"flex-1 bg-gray-900 items-center p-4 py-10"}>
+    <SafeAreaView className={"flex-1 items-center p-4 py-10"}>
       <Pressable className={"absolute top-12 left-4 rounded-full p-2 bg-slate-800/80"} onPress={() => router.back()}>
         <Entypo name="chevron-down" size={24} color="white" />
       </Pressable>
@@ -20,23 +25,28 @@ export default function Player() {
       <View className={"gap-6 flex-1 justify-end w-full mb-8"}>
         <Text className="text-white text-2xl font-bold text-center">{book.title}</Text>
 
-        <PlaybackBar value={0.21} />
+        <PlaybackBar currentTime={playerStatus.currentTime} duration={playerStatus.duration} />
 
         <View className="flex-row items-center justify-between mt-4">
           <Pressable className="p-2">
-            <Ionicons name="play-skip-back" size={28} color="white" />
+            <Ionicons name="play-skip-back" size={24} color="white" />
           </Pressable>
           <Pressable className="p-2">
-            <Ionicons name="play-back" size={28} color="white" />
+            <Ionicons name="play-back" size={24} color="white" />
           </Pressable>
-          <Pressable className="p-4 bg-white/10 rounded-full">
-            <Ionicons name="play" size={54} color="white" />
+
+          <Pressable
+            className="p-4 bg-white/10 rounded-full"
+            onPress={() => (playerStatus.playing ? player.pause() : player.play())}
+          >
+            <Ionicons name={playerStatus.playing ? "pause" : "play"} size={50} color="white" />
+          </Pressable>
+
+          <Pressable className="p-2">
+            <Ionicons name="play-forward" size={24} color="white" />
           </Pressable>
           <Pressable className="p-2">
-            <Ionicons name="play-forward" size={28} color="white" />
-          </Pressable>
-          <Pressable className="p-2">
-            <Ionicons name="play-skip-forward" size={28} color="white" />
+            <Ionicons name="play-skip-forward" size={24} color="white" />
           </Pressable>
         </View>
       </View>
--- src/components/PlaybackBar.tsx
+++ src/components/PlaybackBar.tsx
@@ -1,13 +1,24 @@
-import { View } from "react-native";
+import { formatTime } from "@/utils";
+import { Text, View } from "react-native";
 
-export default function PlaybackBar({ value }: { value: number }) {
+type PlaybackBarType = { currentTime: number; duration: number };
+
+export default function PlaybackBar({ currentTime, duration }: PlaybackBarType) {
+  const progress = currentTime / duration;
+
   return (
-    <View className="w-full bg-slate-900 h-2 rounded-full justify-center">
-      <View className="bg-orange-400 h-full rounded-full" style={{ width: `${value * 100}%` }} />
-      <View
-        className="absolute w-3 h-3 -translate-x-1/2 rounded-full bg-orange-400"
-        style={{ left: `${value * 100}%` }}
-      />
+    <View className="w-full flex-col gap-2">
+      <View className="w-full bg-slate-900 h-2 rounded-full justify-center">
+        <View className="bg-orange-400 h-full rounded-full" style={{ width: `${progress * 100}%` }} />
+        <View
+          className="absolute w-3 h-3 -translate-x-1/2 rounded-full bg-orange-400"
+          style={{ left: `${progress * 100}%` }}
+        />
+      </View>
+      <View className="flex-row justify-between">
+        <Text className="text-gray-400 text-sm">{formatTime(currentTime)}</Text>
+        <Text className="text-gray-400 text-sm">{formatTime(duration)}</Text>
+      </View>
     </View>
   );
 }
